00b9ca95cf45b2a24359b0a826054606c1473487
glitton <glcodeworks@gmail.com>
2023-07-17 19:42:05 -0700

    company filter still wip

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -2,21 +2,17 @@ import { getElement } from "../utils.js";
 import display from "../displayProducts.js";
 
 const setupCompanies = (store) => {
-  let companies = [
-    "all",
-    ...new Set(
-      store.map((product) => {
-        return product.company;
-      })
-    ),
-  ];
+  let companies = ["all", ...new Set(store.map((product) => product.company))];
+
   const companiesDOM = getElement(".companies");
+
   companiesDOM.innerHTML = companies
     .map((company) => {
       return `<button class="company-btn">${company}</button>`;
     })
     .join("");
-  companiesDOM.addEventListener("click", (e) => {
+
+  companiesDOM.addEventListener("click", function (e) {
     const element = e.target;
     if (element.classList.contains("company-btn")) {
       let newStore = [];
