5e65b30acd3faa853dac944fae2c87b665e73af1
glitton <glcodeworks@gmail.com>
2023-07-23 15:34:48 -0700

    set up of single product page complete

--- src/pages/product.js
+++ src/pages/product.js
@@ -28,6 +28,27 @@ window.addEventListener("DOMContentLoaded", async () => {
     const response = await fetch(`${singleProductUrl}${urlID}`);
     if (response.status >= 200 && response.status <= 299) {
       const product = await response.json();
+      // destructure data from object
+      const { id, fields } = product;
+      productID = id;
+
+      const { name, company, price, colors, description } = fields;
+      const image = fields.image[0].thumbnails.large.url;
+
+      // set values
+      document.title = `${name.toUpperCase()} | Comfy`;
+      pageTitleDOM.textContent = `Home | ${name}`;
+      imgDOM.src = image;
+      titleDOM.textContent = name;
+      companyDOM.textContent = `by ${company}`;
+      priceDOM.textContent = formatPrice(price);
+      descDOM.textContent = description;
+      colors.forEach((color) => {
+        const span = document.createElement("span");
+        span.classList.add("product-color");
+        span.style.backgroundColor = `${color}`;
+        colorsDOM.appendChild(span);
+      });
     } else {
       console.log(response.status, response.statusText);
       centerDOM.innerHTML = `<div><h3 class='error'>sorry, something went wrong</h3>
@@ -39,4 +60,8 @@ window.addEventListener("DOMContentLoaded", async () => {
   }
 
   loading.style.display = "none";
+});
+
+cartBtn.addEventListener("click", () => {
+  addToCart(productID);
 });
