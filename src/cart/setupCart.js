d6d39986e8565d7c974fa89993942be03101857a
glitton <glcodeworks@gmail.com>
2023-07-24 16:34:15 -0700

    setupcart init function done

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -50,8 +50,21 @@ const displayCartTotal = () => {
   }, 0);
   cartTotalDOM.textContent = `Total :  ${formatPrice(total)}`;
 };
+const displayCartItemsDOM = () => {
+  cart.forEach((cartItem) => {
+    addToCartDOM(cartItem);
+  });
+};
+const setupCartFunctionality = () => {};
 
 const init = () => {
-  console.log(cart);
+  // display amount of cart items
+  displayCartItemCount();
+  // display total
+  displayCartTotal();
+  // add all cart itmes to the DOM
+  displayCartItemsDOM();
+  // setup cart functionality
+  setupCartFunctionality();
 };
 init();
