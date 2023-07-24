4afdc7a8949cdfa8fb79a60206a0beb27ba42cce
glitton <glcodeworks@gmail.com>
2023-07-23 21:44:11 -0700

    cart totals wip

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -10,7 +10,32 @@ import { findProduct } from "../store.js";
 import addToCartDOM from "./addToCartDOM.js";
 // set items
 
+const cartItemCountDOM = getElement(".cart-item-count");
+const cartItemsDOM = getElement(".cart-items");
+const cartTotalDOM = getElement(".cart-total");
+
+// set up data
+let cart = getStorageItem("cart");
+
 export const addToCart = (id) => {
-  console.log(id);
+  let item = cart.find((cartItem) => cartItem.id === id);
+  if (!item) {
+    let product = findProduct(id);
+    // add product to cart
+    product = { ...product, amount: 1 };
+    cart = [...cart, product];
+    // add product to the DOM
+    addToCartDOM(product);
+    // console.log("this is the cart", cart);
+  } else {
+    // update values
+  }
+
+  // more stuff later
   openCart();
 };
+
+const init = () => {
+  console.log(cart);
+};
+init();
